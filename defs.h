0aeb59cff0e20ec5a11d38eff33132890727ac08
Fatema Lokhandwala <116671656+fatemalokhand@users.noreply.github.com>
2024-01-16 22:05:07 -0500

    Update defs.h

--- defs.h
+++ defs.h
@@ -8,13 +8,12 @@
 #include <semaphore.h>
 
 #define MAX_STR            64
-#define FEAR_RATE           1
+#define MAX_ARR            16
+#define FEAR           	    1
+#define BOREDOM            99
 #define MAX_FEAR          100
 #define MAX_HUNTERS         4
 #define USLEEP_TIME     50000
-#define BOREDOM_MAX        99
-#define MAX_ARR            16
-
 
 typedef enum { EMF, TEMPERATURE, FINGERPRINTS, SOUND } EvidenceClassType;
 typedef enum { POLTERGEIST, BANSHEE, BULLIES, PHANTOM } GhostClassType;
@@ -26,17 +25,15 @@ typedef struct EvidenceType{
   int isGhostly;
 }EvidenceType;
 
-// declaring evidence node type
-typedef struct EvidenceNodeType{
-  EvidenceType* data;
-  struct EvidenceNodeType *nextNode;
-}EvidenceNodeType;
-
-// declaring edge list type
-typedef struct EdgeListType {
-  struct RoomNodeType * headNode;
-  int size;
-} EdgeListType;
+// declaring room type
+typedef struct RoomType{
+  sem_t *mutex;
+  char name[MAX_STR];
+  EvidenceListType evidenceList;
+  EdgeListType adjacentRooms;
+  struct GhostType * ghost;
+  struct HunterType* hunters[4];
+} RoomType;
 
 // declaring evidence list type
 typedef struct EvidenceListType{
@@ -58,16 +55,6 @@ typedef struct HunterType{
   struct GhostType* ghost;
 } HunterType;
 
-// declaring room type
-typedef struct RoomType{
-  sem_t *mutex;
-  char name[MAX_STR];
-  EvidenceListType evidenceList;
-  EdgeListType adjacentRooms;
-  struct GhostType * ghost;
-  struct HunterType* hunters[4];
-} RoomType;
-
 // declaring ghost type
 typedef struct GhostType{
   RoomType* currentRoom;
@@ -83,6 +70,18 @@ typedef struct RoomNodeType {
   struct RoomNodeType* nextNode;
 }RoomNodeType;
 
+// declaring edge list type
+typedef struct EdgeListType {
+  struct RoomNodeType * headNode;
+  int size;
+} EdgeListType;
+
+// declaring evidence node type
+typedef struct EvidenceNodeType{
+  EvidenceType* data;
+  struct EvidenceNodeType *nextNode;
+}EvidenceNodeType;
+
 // declaring room list type
 typedef struct  {
   RoomNodeType *headNode;
@@ -97,25 +96,20 @@ typedef struct Building {
 
 
 // function declarations
-int randInt(int, int);          
-float randFloat(float, float);  
-void getNames(char*, char*, char*, char*);
-void trim(char*);
 
+// room functions
 void initRoomList(RoomListType *);
 void addRoom(RoomListType  *, RoomNodeType *);
 void populateRooms(BuildingType*);   
 void initRoom(RoomType*, char*); 
-int doseRoomContainGhost(RoomType*);
+int doesRoomContainGhost(RoomType*);
 void printRoom(RoomType *);
 void connectRooms(RoomType *, RoomType *);
 void getNewRoom(RoomType **);
 void printRoomOccupants(RoomType *, char[]);
 int getNumberOfHuntersInRoom(RoomType *);
 
-void initBuilding(BuildingType*); 
-void printBuilding(BuildingType*); 
-
+// ghost functions
 void initGhost(GhostType *, RoomType *, unsigned int);
 RoomType * getRandomStartingRoom(unsigned int, BuildingType *);
 void moveGhost(GhostType *);
@@ -123,6 +117,7 @@ void printGhost(GhostType *);
 void ghostAction(GhostType *, unsigned int);
 GhostClassType randomGhost();
 
+// hunter functions
 void initHunter(HunterType *, char *, char *, RoomType *, EvidenceClassType, int, unsigned int, GhostType*);
 void printHunter(HunterType *);
 void moveHunter(HunterType *);
@@ -131,9 +126,20 @@ void communicateEvidence(HunterType *);
 void hunterAction(HunterType *);
 void communicate(HunterType *);
 
+// evidence functions
 void initEvidence(EvidenceType*, unsigned int);
 void printEvidence(EvidenceType*);
 void initEvidenceList(EvidenceListType *);
 void addEvidence(EvidenceListType*, EvidenceType*);
 void deleteEvidence(EvidenceListType*, EvidenceType*);
 void printEvidenceList(EvidenceListType*);
+
+// other helper functions
+int randInt(int, int);          
+float randFloat(float, float);  
+void getNames(char*, char*, char*, char*);
+void trim(char*);
+
+// building functions
+void initBuilding(BuildingType*); 
+void printBuilding(BuildingType*); 
