eb79f70b68008ab4b3c69c6fbc87559067ea84a6
Fatema Lokhandwala <116671656+fatemalokhand@users.noreply.github.com>
2024-01-16 22:10:48 -0500

    Update hunter.c

--- hunter.c
+++ hunter.c
@@ -1,156 +1,143 @@
 #include "defs.h"
 
 /*Function: initHunter()
-  Purpose:  initHunter
-       in:  HunterType * p_hunterType, char * p_name, char * p_nickName, RoomType * p_startRoom, EvidenceClassType evidenceClass, int arrayIndex, unsigned int seed, GhostType* p_ghostType
+  Purpose:  initializing the hunter
+       in:  HunterType* hunterType, char* name, char* nickName, RoomType* startRoom, EvidenceClassType evidenceClass, int arrayIndex, unsigned int seed, GhostType* ghostType
 */
-void initHunter(HunterType * p_hunterType, char * p_name, char * p_nickName, RoomType * p_startRoom, EvidenceClassType evidenceClass, int arrayIndex, unsigned int seed, GhostType* p_ghostType)
+void initHunter(HunterType* hunterType, char* name, char* nickName, RoomType* startRoom, EvidenceClassType evidenceClass, int arrayIndex, unsigned int seed, GhostType* ghostType)
 {
-  p_hunterType->p_currentRoom = p_startRoom;
-  p_hunterType->p_currentRoom->hunters[arrayIndex] = p_hunterType;
-  strcpy(p_hunterType->name, p_name);
-  strcpy(p_hunterType->nickName, p_nickName);
-  initEvidenceList(&(p_hunterType->evidenceList));
-  p_hunterType->evidenceClassType = evidenceClass;
-  p_hunterType->fear = 0;
-  p_hunterType->boredom = BOREDOM_MAX;
-  p_hunterType->arrayIndex = arrayIndex;
-  p_hunterType->seed = seed;
-  p_hunterType->p_ghost = p_ghostType;
-  p_hunterType->ghostlyEvidenceCount = 0;
+  hunterType->currentRoom = startRoom;
+  hunterType->currentRoom->hunters[arrayIndex] = hunterType;
+  strcpy(hunterType->name, name);
+  strcpy(hunterType->nickName, nickName);
+  initEvidenceList(&(hunterType->evidenceList));
+  hunterType->evidenceClassType = evidenceClass;
+  hunterType->fear = 0;
+  hunterType->boredom = BOREDOM;
+  hunterType->arrayIndex = arrayIndex;
+  hunterType->seed = seed;
+  hunterType->ghost = ghostType;
+  hunterType->ghostlyEvidenceCount = 0;
 }
 
 /*Function: printHunter()
-  Purpose:  print single Hunter
-       in:  HunterType * p_hunterType
+  Purpose:  printing out a single Hunter
+       in:  HunterType* hunterType
 */
-void printHunter(HunterType * p_hunterType)
+void printHunter(HunterType* hunterType)
 {
-  printf("Hunter(Name:%s, NickName:%s, CurrentRoom:%s, EvidenceClassType:%d, Fear:%d, ArrayIndex:%d)", p_hunterType->name, p_hunterType->nickName, p_hunterType->p_currentRoom->name, p_hunterType->evidenceClassType, p_hunterType->fear, p_hunterType->arrayIndex);
+  printf("Hunter (Name:%s, NickName:%s, CurrentRoom:%s, EvidenceClassType:%d, Fear:%d, ArrayIndex:%d)", hunterType->name, hunterType->nickName, hunterType->currentRoom->name, hunterType->evidenceClassType, hunterType->fear, hunterType->arrayIndex);
 }
 
-void moveHunter(HunterType * p_hunterType)
+void moveHunter(HunterType* hunterType)
 {
-  p_hunterType->p_currentRoom->hunters[p_hunterType->arrayIndex] = NULL;//tell the hunters current room we are leaving
+  // the room that the hunters are leaving from 
+  hunterType->currentRoom->hunters[hunterType->arrayIndex] = NULL;
 
+  //collecting the evidence
+  getNewRoom(&(hunterType->currentRoom));
 
-  //printf("HunterMovement: %s ====>", p_hunterType->p_currentRoom->name);
-
-  //collect evidence
-
-  getNewRoom(&(p_hunterType->p_currentRoom));
-
-
-  printf("%s moved ====> %s \n", p_hunterType->name, p_hunterType->p_currentRoom->name);
-
-
-  p_hunterType->p_currentRoom->hunters[p_hunterType->arrayIndex] = p_hunterType;//tell the hunters new room we are now here
+  // the new room that the hunters are currently in
+  hunterType->currentRoom->hunters[hunterType->arrayIndex] = hunterType;
 }
 
 /*Function: collectEvidence()
-  Purpose:  collect the Evidence
+  Purpose:  collecting the Evidence
        in:  pointer to the hunter
 */
-void collectEvidence(HunterType * p_hunterType)
+void collectEvidence(HunterType* hunterType)
 {
-  //printf("Hunter Looked for Evidence ====>");
   int found = 0;
 
-  if(p_hunterType->p_currentRoom->evidenceList.p_headNode != NULL)
+  if(hunterType->currentRoom->evidenceList.headNode != NULL)
   {
-    EvidenceNodeType* p_currentEvidenceNode;
-    p_currentEvidenceNode = p_hunterType->p_currentRoom->evidenceList.p_headNode;
+    EvidenceNodeType* currentEvidenceNode;
+    currentEvidenceNode = hunterType->currentRoom->evidenceList.headNode;
 
-    while(p_currentEvidenceNode != NULL)
+    while(currentEvidenceNode != NULL)
     {
-        if(p_currentEvidenceNode->p_data->evidenceClassType == p_hunterType->evidenceClassType)
+        if(currentEvidenceNode->data->evidenceClassType == hunterType->evidenceClassType)
         {
-            appendEvidence(&(p_hunterType->evidenceList), p_currentEvidenceNode->p_data);
+            addEvidence(&(hunterType->evidenceList), currentEvidenceNode->data);
 
-            if(p_currentEvidenceNode->p_data->isGhostly == 1){
-              p_hunterType->ghostlyEvidenceCount++;
+            if(currentEvidenceNode->data->isGhostly == 1){
+              hunterType->ghostlyEvidenceCount++;
             }
 
-            deleteEvidence(&(p_hunterType->p_currentRoom->evidenceList), p_currentEvidenceNode->p_data);
-            //have the room delite the current evidence
-            //p_hunterType->arrayIndex++;
+            deleteEvidence(&(hunterType->currentRoom->evidenceList), currentEvidenceNode->data);
         }
 
-        if(p_currentEvidenceNode->p_nextNode != NULL)
+        if(currentEvidenceNode->nextNode != NULL)
         {
-          p_currentEvidenceNode = p_currentEvidenceNode->p_nextNode;
+          currentEvidenceNode = currentEvidenceNode->nextNode;
         }
         else
         {
-          p_currentEvidenceNode = NULL;
+          currentEvidenceNode = NULL;
         }
     }
-
-
   }
-
-
-
-  //printf("Hunter Looked for Evidence ====> found[%d] \n", found);
-  printf("%s Looked for Evidence ====>%d \n", p_hunterType->name, found);
 }
 
 /*Function: communicateEvidence()
-  Purpose:  hunters communicate Evidence
+  Purpose:  hunters communicating about the collected Evidence
        in:  pointer to the hunter
 */
-void communicateEvidence(HunterType * p_hunterType)
+void communicateEvidence(HunterType* hunterType)
 {
   printf("< CommunicateEvidence >");
 }
 
 /*Function: hunterAction()
-  Purpose:  randomly chose hunter action
+  Purpose:  randomly choose an action for the hunter to take
        in:  pointer to the hunter
 */
-void hunterAction(HunterType * p_hunterType)
+void hunterAction(HunterType* hunterType)
 {
-  p_hunterType->boredom =  p_hunterType->boredom - 1;
+  hunterType->boredom =  hunterType->boredom - 1;
 
 
-  //When the hunter is in a room with the ghost
-  if(doseRoomContainGhost(p_hunterType->p_currentRoom) == 1)
+  // When the hunter is in a room with the ghost
+  if(doesRoomContainGhost(hunterType->currentRoom) == 1)
   {
-    p_hunterType->fear = p_hunterType->fear + FEAR_RATE;
-    p_hunterType->boredom = BOREDOM_MAX;
+    hunterType->fear = hunterType->fear + FEAR;
+    hunterType->boredom = BOREDOM;
   }
 
 
   int actionRangeMax = 1;
-  if(getNumberOfHuntersInRoom(p_hunterType->p_currentRoom) > 1){ actionRangeMax = 2; }
-  int randomActionInt = (rand() % actionRangeMax);//Note the range of newRoomInt is [0, pp_currentRoom->size]
-  //int randomActionInt = 2;
-  switch (randomActionInt)
+  if(getNumberOfHuntersInRoom(hunterType->currentRoom) > 1){ actionRangeMax = 2; }
+  int randomActionInt = (rand() % actionRangeMax);
+  
+  switch(randomActionInt)
   {
-    case 0://Move
-      moveHunter(p_hunterType);
+    //Move to a different room
+    case 0:
+      moveHunter(hunterType);
       break;
-    case 1://CollectEvidence
-      collectEvidence(p_hunterType);
+    //Collect evidence
+    case 1:
+      collectEvidence(hunterType);
       break;
-    case 2://CommunicateEvidence
+    //Communicate evidence
+    case 2:
         printf("hunters communitcate\n");
-        communicate(p_hunterType);
+        communicate(hunterType);
         break;
   }
 
-  if(p_hunterType->ghostlyEvidenceCount >= 3){
-    p_hunterType->p_ghost->busted = 1;
+  if(hunterType->ghostlyEvidenceCount >= 3){
+    hunterType->ghost->busted = 1;
   }
 
 }
 
-void communicate(HunterType *p_hunterType){
+void communicate(HunterType *hunterType){
 
-  for(int i = 0; i < 4; i++){
+  for(int i=0; i<4; i++){
 
-    if(p_hunterType->p_currentRoom->hunters[i] != NULL && p_hunterType->p_currentRoom->hunters[i]->evidenceList.p_headNode != NULL){
-      appendEvidence(&(p_hunterType->evidenceList),p_hunterType->p_currentRoom->hunters[i]->evidenceList.p_headNode->p_data);
+    if(hunterType->currentRoom->hunters[i] != NULL && hunterType->currentRoom->hunters[i]->evidenceList.headNode != NULL){
+      addEvidence(&(hunterType->evidenceList), hunterType->currentRoom->hunters[i]->evidenceList.headNode->data);
     }
   }
 }
